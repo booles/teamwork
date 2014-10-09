3f6c6718d8d057ceb2452420c69e71bafe17479c
booles <wangyunhenan168@163.com>
2014-10-09 11:22:56 +0800

    teamwork

---
+++ app.js
@@ -0,0 +1,68 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , ejs = require('ejs');
+var partials = require('express-partials');
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views/ejs');
+app.set('view engine', "ejs");
+app.use(partials()); 
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+app.get('/login', user.login);
+
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+var users = {}
+
+io.sockets.on('connection', function (socket) {
+
+    /*socket.emit("socket",socket);*/  //当试图发送socket时会出现 RangeError: Maximum call stack size exceeded
+    socket.on("online",function (data){
+        users[socket.id] = data;        //保存用户名
+        io.sockets.emit('online', users);   //发给所有人用户名 包括自己
+    });
+    //说的话发给所有人，不包括自己
+    socket.on("say",function (data){
+        socket.broadcast.emit('say', data);
+        //var clients = io.sockets.clients();
+        //console.log(clients);    
+    });
+    //掉线
+    socket.on('disconnect', function() {
+        if(users[socket.id]){
+            delete users[socket.id];
+            io.sockets.emit('online', users);
+        };
+    });
+});
+
+
+
+
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ public/javascripts/socketApp.js
@@ -0,0 +1,53 @@
+
+var socket = null;
+
+
+angular.module('socketApp', ["ngAnimate"])
+	.controller('socketCtrl', ['$scope', function ($scope) {
+		$scope.showed = false;
+
+		//存储聊天信息
+		$scope.chartText = [];
+
+		$scope.userNames = "登陆";
+
+		//接收所有在线的
+		$scope.allUser = {};
+
+		$scope.login = function (){
+			$scope.userNames = this.userName;
+			$scope.showed = !$scope.showed;	
+			//只有当登陆的时候连接socket
+			socket = io.connect('http://localhost:3000');
+			//上线了，接收到发过来的用户名，包括自己
+			socket.on('online', function (data) {
+				$scope.allUser = data;
+				$scope.$apply();
+			});
+			//发送用户名
+			socket.emit("online",$scope.userNames);
+			//接受说的话
+			socket.on("say",function (data){
+				$scope.chartText.unshift(data);
+				$scope.$apply();
+			});
+		};
+
+		$scope.isUserNames = function (userNames){
+			return userNames == "登陆";	
+		};
+		$scope.sendText = function (){
+			if(!$scope.textarea) return;
+			$scope.date = new Date().getTime();
+			//把时间，用户和内容发送后台
+			$scope.textJson = {
+				time:$scope.date,
+				user:$scope.userNames,
+				sayText:$scope.textarea
+			};
+			$scope.chartText.unshift($scope.textJson);
+			$scope.textarea = "";
+			//发送说的话
+			socket.emit("say",$scope.textJson);
+		};
+	}])
